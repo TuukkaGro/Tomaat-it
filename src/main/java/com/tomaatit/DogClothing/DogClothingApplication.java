6c653c8f4f809d81308e9df5ae324ad87d4440d0
Tuukka Grönholm <tmgronholm@gmail.com>
2022-10-27 12:19:35 +0300

    First commit for tomaatit

---
+++ src/main/java/com/tomaatit/DogClothing/DogClothingApplication.java
@@ -0,0 +1,13 @@
+package com.tomaatit.DogClothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogClothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogClothingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomaatit/DogClothing/DogClothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomaatit.DogClothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogClothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
