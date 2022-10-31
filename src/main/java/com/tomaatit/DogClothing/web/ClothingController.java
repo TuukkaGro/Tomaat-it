74ee7bf258ca385cb8206694df8154408f3c8dfc
Alfred <alfred.ahonen@gmail.com>
2022-10-31 09:44:58 +0200

    controller päivitetty, index sivu luotu

--- src/main/java/com/tomaatit/DogClothing/web/ClothingController.java
+++ src/main/java/com/tomaatit/DogClothing/web/ClothingController.java
@@ -1,5 +1,27 @@
 package com.tomaatit.DogClothing.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tomaatit.DogClothing.domain.ClothingRepository;
+
+@Controller
 public class ClothingController {
+	@Autowired
+	private ClothingRepository repository;
+	
+	@RequestMapping(value= {"/", "/home"})
+	public String frontPage(Model model) {
+		return "index";
+	}
+	
+	@RequestMapping(value= {"/clothinglist"})
+	public String clothingList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "clothinglist";
+	}
+	
 
 }
