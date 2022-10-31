138c3cde0b8c86f4aeb683e2b102927b524d94a5
Tiina Viertola <bdh538@myy.haaga-helia.fi>
2022-10-31 10:01:43 +0200

    updateme.txt adds

--- src/main/java/com/tomaatit/DogClothing/domain/Clothing.java
+++ src/main/java/com/tomaatit/DogClothing/domain/Clothing.java
@@ -7,44 +7,45 @@ import javax.persistence.Id;
 
 @Entity
 public class Clothing {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	
+
 	private Long id;
 	private String name;
 	private double price;
 	private String producer;
-	
-	
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public String getProducer() {
 		return producer;
 	}
+
 	public void setProducer(String producer) {
 		this.producer = producer;
 	}
-	
-	
-	
-	
 
 }
