dcd3f704513323f7d19e33f4fa36418c48f220cc
LriKrj <lauri.karjalainen3@hotmail.com>
2022-10-31 10:08:31 +0200

    commandline runner

--- src/main/java/com/tomaatit/DogClothing/domain/Clothing.java
+++ src/main/java/com/tomaatit/DogClothing/domain/Clothing.java
@@ -13,35 +13,73 @@ public class Clothing {
 	
 	private Long id;
 	private String name;
+	private String type;
 	private double price;
 	private String producer;
 	
-	
-	
+	public Clothing(Long id, String name, String type, double price, String producer) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.price = price;
+		this.producer = producer;
+	}
+
+	public Clothing(String name, String type, double price, String producer) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.price = price;
+		this.producer = producer;
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public String getProducer() {
 		return producer;
 	}
+
 	public void setProducer(String producer) {
 		this.producer = producer;
 	}
+
+	@Override
+	public String toString() {
+		return "Clothing [id=" + id + ", name=" + name + ", type=" + type + ", price=" + price + ", producer="
+				+ producer + "]";
+	}
+	
 	
 	
 	
