0d9f7f173107da0d2247a3030ff2645a20d29359
Alfred <alfred.ahonen@gmail.com>
2022-11-17 12:37:07 +0200

    cors

--- src/main/java/com/tomaatit/DogClothing/web/ClothingRestController.java
+++ src/main/java/com/tomaatit/DogClothing/web/ClothingRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.tomaatit.DogClothing.domain.ClothingRepository;
 import com.tomaatit.DogClothing.domain.Producer;
 import com.tomaatit.DogClothing.domain.ProducerRepository;
 
+@CrossOrigin(origins= "http://localhost:3000/", maxAge = 3600)
 @RestController
 public class ClothingRestController {
 	@Autowired
