4cdc268a232600101fa05f9c7dc5765d699e5425
Tiina Viertola <bdh538@myy.haaga-helia.fi>
2022-12-05 09:11:19 +0200

    added some comments

--- src/main/java/com/tomaatit/DogClothing/web/ClothingRestController.java
+++ src/main/java/com/tomaatit/DogClothing/web/ClothingRestController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -76,7 +75,6 @@ public class ClothingRestController {
 
 	// save a new entry to producers
 	@PostMapping("/api/producers")
-	@PreAuthorize("hasAuthority('ADMIN')")
 	Producer newProducer(@RequestBody Producer newProducer) {
 		return prepository.save(newProducer);
 	}
